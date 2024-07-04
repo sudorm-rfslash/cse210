38a2e0ff652f1f7713d9ff8b61608ad0ea98f1d4
sudorm-rfslash <coupe-00-daylong@icloud.com>
2024-07-04 15:55:52 -0600

    changed resume.cs to use the job.showjobinfo().

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -7,7 +7,7 @@ public class Resume
     Console.WriteLine($"Name: {_name}");
     Console.WriteLine($"Jobs:");
     foreach (Job j in _jobs){
-      Console.WriteLine($"{j._jobTitle} ({j._company}) {j._startYear}-{j._endYear}");
+      j.showJobInfo();
     }
   }
 
